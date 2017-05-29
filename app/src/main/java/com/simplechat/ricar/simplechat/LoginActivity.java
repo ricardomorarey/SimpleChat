a45b242dbc3304db7bf6016d86c3b24a3d669955
Ricardo Gonzalez Mora-Rey <ricardomorarey@gmail.com>
2017-05-29 10:32:25 +0200

    ultima clase firebase añadido onstart y onstop.

--- app/src/main/java/com/simplechat/ricar/simplechat/LoginActivity.java
+++ app/src/main/java/com/simplechat/ricar/simplechat/LoginActivity.java
@@ -1,14 +1,23 @@
 package com.simplechat.ricar.simplechat;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText username, pasword;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +27,34 @@ public class LoginActivity extends AppCompatActivity {
         username = (EditText)findViewById(R.id.etUser);
         pasword = (EditText)findViewById(R.id.etPass);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null){
+                    Toast.makeText(LoginActivity.this, "El usuario ya ha iniciado sesion", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(LoginActivity.this, "El usuario salió de la sesión", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (authStateListener != null){
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
     }
 
     public void login(View view) {
